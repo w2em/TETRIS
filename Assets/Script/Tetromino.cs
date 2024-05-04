89008afbceb6c84e1feb082627b1520722479fb7
w2em <137866003+w2em@users.noreply.github.com>
2024-05-04 12:08:06 +0900

    ププログラムを進めました。

---
+++ Assets/Script/Tetromino.cs
@@ -0,0 +1,19 @@
+using UnityEngine.Tilemaps;
+
+public enum Tetromino
+{
+    I,
+    O,
+    T,
+    J,
+    L,
+    S,
+    Z
+}
+
+[System.Serializable]
+public struct TetrominiData
+{
+    public Tetromino tetromino;
+    public Tile tile;
+}
