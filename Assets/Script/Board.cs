26bf3b4bb1012355fea72f57584e16556fbce3c8
w2em <137866003+w2em@users.noreply.github.com>
2024-05-13 12:46:44 +0900

    40分まであと少し

--- Assets/Script/Board.cs
+++ Assets/Script/Board.cs
@@ -1,6 +1,34 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class Broad : MonoBehaviour
+public class Board : MonoBehaviour
 {
+    public Tilemap tilemap { get; private set; }
     public TetrominiData[] tetrominies;
+
+    private void Awake()
+    {
+        this.tilemap = GetComponentInChildren<Tilemap>();
+
+        for (int i = 0;i<this.tetrominies.Length;i++)
+        {
+            this.tetrominies[i].Initialize();
+        }
+    }
+
+    private void Start()
+    {
+        SpawnPiece();
+    }
+
+    public void SpawnPiece()
+    {
+        int random = Random.Range(0, tetrominies.Length);
+        TetrominiData data = this.tetrominies[random];
+    }
+
+    public void Set()
+    {
+
+    }
 }
