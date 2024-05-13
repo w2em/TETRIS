2a4d6443371c8a608739b4022ada6770a210dfd2
w2em <137866003+w2em@users.noreply.github.com>
2024-05-13 16:31:25 +0900

    スポーンまで出来ました(o^―^o)ﾆｺ

--- Assets/Script/Board.cs
+++ Assets/Script/Board.cs
@@ -4,11 +4,14 @@ using UnityEngine.Tilemaps;
 public class Board : MonoBehaviour
 {
     public Tilemap tilemap { get; private set; }
+    public Piece activePiece { get; private set; }
     public TetrominiData[] tetrominies;
+    public Vector3Int spawnPosition;
 
     private void Awake()
     {
         this.tilemap = GetComponentInChildren<Tilemap>();
+        this.activePiece = GetComponentInChildren<Piece>();
 
         for (int i = 0;i<this.tetrominies.Length;i++)
         {
@@ -25,10 +28,17 @@ public class Board : MonoBehaviour
     {
         int random = Random.Range(0, tetrominies.Length);
         TetrominiData data = this.tetrominies[random];
+
+        this.activePiece.Initialize(this,this.spawnPosition, data);
+        Set(this.activePiece);
     }
 
-    public void Set()
+    public void Set(Piece piece)
     {
-
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePosition,piece.data.tile);
+        }
     }
 }
